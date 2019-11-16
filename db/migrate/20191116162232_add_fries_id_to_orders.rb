de07dd7009a7d626c544374dec70ea6603952d52
FredrickBarrett <f.barrett12@gmail.com>
2019-11-16 11:24:51 -0500

    added foreign keys to orders

---
+++ db/migrate/20191116162232_add_fries_id_to_orders.rb
@@ -0,0 +1,5 @@
+class AddFriesIdToOrders < ActiveRecord::Migration[6.0]
+  def change
+    add_column :orders, :fries_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_16_155943) do
+ActiveRecord::Schema.define(version: 2019_11_16_162232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_11_16_155943) do
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "fries_id"
   end
 
   create_table "users", force: :cascade do |t|
